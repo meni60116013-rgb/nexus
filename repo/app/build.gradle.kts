9b4f3b90feef59baeebdc09c6a1a591b9d5d0640
meni60116013-rgb <meni60116013@gmail.com>
2026-08-31 17:00:23 -0600

    fix: corregir tema inexistente Theme.Material3.Dark -> Theme.Material3.DayNight

--- repo/app/build.gradle.kts
+++ repo/app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
     kotlinOptions { jvmTarget = "17" }
 }
 dependencies {
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
